c7e19333483c8841363b01a1c3fa5d7b3c9c117f
krturner <krisleoturner@gmail.com>
2020-10-02 14:32:33 -0600

    more changes, what the heck

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,7 @@ export default function Index() {
       <button onClick={(e) => auth.signout()}>Sign Out</button>
     </div>
   ) : (
-    <div>
       <button onClick={(e) => auth.signinWithGitHub()}>Sign In With GitHub </button>
-      <button onClick={(e) => auth.signinWithGoogle()}>Sign In With Google </button>
-    </div>
   )
 }
 
